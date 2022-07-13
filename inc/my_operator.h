be02a3cee27aa76bbb5f9d18c3aeed7e10a25e2d
Veneta <venkireva80@gmail.com>
2022-07-13 19:49:43 +0300

    Add Big 4 to MyOperator class, update documentation and remove other classes from files

--- inc/my_operator.h
+++ inc/my_operator.h
@@ -6,74 +6,18 @@
 
 using std::vector;
 
-/**
- * @brief operator base class
- * 
- */
+/// Operator interface
 struct MyOperator {
     char symbol; // the symbol of the operator
     int priority; // the priority of the operator
 
-    MyOperator(char sym, int pri) {
-        symbol = sym;
-        priority = pri;
-    }
+    MyOperator();
+    MyOperator(char _symbol, int _priority);
+    MyOperator(MyOperator const& other);
+    MyOperator& operator=(MyOperator const& other);
     
     virtual int calculate(int left, int right) const = 0;
-    virtual ~MyOperator() {}
-};
-
-/**
- * @brief manages and picks available operators
- * 
- */
-class OperatorPicker 
-{
-    vector<const MyOperator*> myOperators; //all registered operators
-
-    OperatorPicker() {};
-    OperatorPicker(const OperatorPicker&) = delete;
-    OperatorPicker& operator= (const OperatorPicker&) = delete;
-
-    const MyOperator* findOperator(char s) {
-        for (const MyOperator* const& current : myOperators) {
-            if (current->symbol == s) {return current;}
-        }
-        return nullptr;
-    }
-
-public:
-
-    static OperatorPicker* instance() {
-        static OperatorPicker i;
-        return &i;
-    }
-
-    void add(const MyOperator* oper) {
-        if (oper && !findOperator(oper->symbol)) {
-            myOperators.push_back(oper);
-        }
-    }
-
-    const MyOperator* pick(char s) {
-        return findOperator(s);
-    }
-};
-
-/**
- * @brief registers operators
- * 
- * @tparam DT operator
- */
-template <typename DT>
-class OperatorSaver {
-private:
-    DT oper; //the operator to be registered
-
-public:
-    OperatorSaver() {
-        OperatorPicker::instance() -> add(&oper);
-    }
+    virtual ~MyOperator();
 };
 
 #endif // __MYOPERATOR_H__
--- src/my_operator.cpp
+++ src/my_operator.cpp
@@ -1,170 +1,31 @@
 #include "../inc/my_operator.h"
 
-/**
- * @brief the + operator
- * 
- */
-struct Addition : public MyOperator
-{
-    Addition() : MyOperator('+', 5) {}
-    int calculate (int right, int left) const override {
-        return left + right;
-    }
-};
-static OperatorSaver<Addition> registrationAddition;
+// Default constructor
+MyOperator::MyOperator() = default;
 
-/**
- * @brief the - operator
- * 
- */
-struct Subtraction : public MyOperator
-{
-    Subtraction() : MyOperator('-', 5) {}
-    int calculate (int right, int left) const override {
-        return left - right;
-    }
-};
-static OperatorSaver<Subtraction> registrationSubtraction;
 
-/**
- * @brief the * operator
- * 
- */
-struct Multiplication : public MyOperator
-{
-    Multiplication() : MyOperator('*', 10) {}
-    int calculate (int right, int left) const override {
-        return left * right;
-    }
-};
-static OperatorSaver<Multiplication> registrationMultiplication;
+/// Constructor with parameters
+/// \param _symbol symbol parameter
+/// \param _priority priority parameter
+MyOperator::MyOperator(char _symbol, int _priority) : symbol(_symbol), priority(_priority) {}
 
-/**
- * @brief the / operator
- * 
- */
-struct Division : public MyOperator
-{
-    Division() : MyOperator('/', 10) {}
-    int calculate (int right, int left) const override {
-        return left / right;
-    }
-};
-static OperatorSaver<Division> registrationDivision;
 
-/**
- * @brief the ( operator
- * 
- */
-struct BracketOpen : public MyOperator
-{
-    BracketOpen() : MyOperator('(', 2) {}
-    int calculate (int right, int left) const override {
-        throw std::runtime_error("Error: trying to calculate (!");
-    }
-};
-static OperatorSaver<BracketOpen> registrationBracketOpen;
+/// Copy constructor
+/// \param other object to copy
+MyOperator::MyOperator(MyOperator const &other) : symbol(other.symbol), priority(other.priority) {}
 
-/**
- * @brief the ) operator
- * 
- */
-struct BracketClose : public MyOperator
-{
-    BracketClose() : MyOperator(')', 50) {}
-    int calculate (int right, int left) const override {
-        throw std::runtime_error("Error: trying to calculate )!");
-    }
-};
-static OperatorSaver<BracketClose> registrationBracketClose;
 
-/**
- * @brief the < operator
- * 
- */
-struct LessThan : public MyOperator
-{
-    LessThan() : MyOperator('<', 0) {}
-    int calculate (int right, int left) const override {
-        return left < right;
+/// Copy assignment operator
+/// \param other object to copy
+/// \return updated object
+MyOperator &MyOperator::operator=(MyOperator const &other) {
+    if (this != &other) {
+        symbol = other.symbol;
+        priority = other.priority;
     }
-};
-static OperatorSaver<LessThan> registrationLessThan;
+    return *this;
+}
 
-/**
- * @brief the > operator
- * 
- */
-struct GreaterThan : public MyOperator
-{
-    GreaterThan() : MyOperator('>', 0) {}
-    int calculate (int right, int left) const override {
-        return left > right;
-    }
-};
-static OperatorSaver<GreaterThan> registrationGreaterThan;
 
-/**
- * @brief the != operator
- * 
- */
-struct NotEqual : public MyOperator
-{
-    NotEqual() : MyOperator('!', 0) {}
-    int calculate (int right, int left) const override {
-        return left != right;
-    }
-};
-static OperatorSaver<NotEqual> registrationNotEqual;
-
-/**
- * @brief the == operator
- * 
- */
-struct Equal : public MyOperator
-{
-    Equal() : MyOperator('=', 0) {}
-    int calculate (int right, int left) const override {
-        return left == right;
-    }
-};
-static OperatorSaver<Equal> registrationEqual;
-
-/**
- * @brief the && operator
- * 
- */
-struct And : public MyOperator
-{
-    And() : MyOperator('&', 1) {}
-    int calculate (int right, int left) const override {
-        return left && right;
-    }
-};
-static OperatorSaver<And> registrationAnd;
-
-/**
- * @brief the || operator
- * 
- */
-struct Or : public MyOperator
-{
-    Or() : MyOperator('|', 1) {}
-    int calculate (int right, int left) const override {
-        return left || right;
-    }
-};
-static OperatorSaver<Or> registrationOr;
-
-/**
- * @brief the ~ operator
- * 
- */
-struct Not : public MyOperator
-{
-    Not() : MyOperator('~', 100) {}
-    int calculate (int right = 0, int left = 0) const override {
-        return right == 0 ? 1 : 0;
-    }
-};
-static OperatorSaver<Not> registrationNot;
+/// Destructor
+MyOperator::~MyOperator() = default;
