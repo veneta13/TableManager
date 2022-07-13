c648a6bb735d785b2d846c8de2256140406aff65
Veneta <venkireva80@gmail.com>
2022-07-13 17:24:06 +0300

    Update documentation for Executor class

--- inc/executor.h
+++ inc/executor.h
@@ -6,38 +6,36 @@
 
 #include <iomanip>
 #include <fstream>
+#include <iostream>
 
 
 /// Executor for user input | Singleton
 class Executor {
     bool exit = false; // flag showing if the program should be terminated
 
-    Executor() = default;
+    Executor();
 
-    void set(vector<string>& args);
-    void printVal(vector<string>& args);
-    void printExpr(vector<string>& args);
-    void printExprAll();
-    void printValAll();
-    void save(vector<string>& args);
-    void load(vector<string>& args);
-    void increase(vector<string>& args);
-    void decrease(vector<string>& args);
+    void set(vector<string>& args) const;
+    void printVal(vector<string>& args) const;
+    void printExpr(vector<string>& args) const;
+    void printExprAll() const;
+    void printValAll() const;
+    void save(vector<string>& args) const;
+    void load(vector<string>& args) const;
+    void increase(vector<string>& args) const;
+    void decrease(vector<string>& args) const;
 
-    void printExpression(Address address);
-    void printValue(Address address);
-    void printColumnLine(int iteration, int times);
+    void printExpression(Address address) const;
+    void printValue(Address address) const;
+    void printColumnLine(int iteration, int times) const;
 
 public:
     Executor(const Executor&) = delete;
     Executor& operator= (const Executor&) = delete;
 
-    static Executor* instance() {
-        static Executor i;
-        return &i;
-    }
+    static Executor* instance();
 
-    bool isExit();
+    bool isExit() const;
     void execute();
 };
 
--- src/executor.cpp
+++ src/executor.cpp
@@ -1,43 +1,42 @@
 #include "../inc/executor.h"
 
-#include <iostream>
+/// Default constructor
+Executor::Executor() = default;
 
-/**
- * @brief generate and execute commands
- * 
- */
+
+/// Creates and executes a user command
 void Executor::execute()
 {
     Command* command = CommandCreator::instance()->createCommand();
 
-    if (command->getName().compare("SET") == 0){
+    if (command->getName() == "SET") {
         set(command->getArguments());
     }
-    else if (command->getName().compare("SAVE") == 0){
+    else if (command->getName() == "SAVE") {
         save(command->getArguments());
     }
-    else if (command->getName().compare("LOAD") == 0){
+    else if (command->getName() == "LOAD"){
         load(command->getArguments());
     }
-    else if (command->getName().compare("PRINT EXPR ALL") == 0){
+    else if (command->getName() == "PRINT EXPR ALL"){
         printExprAll();
     }
-    else if (command->getName().compare("PRINT VAL ALL") == 0){
+    else if (command->getName() == "PRINT VAL ALL"){
         printValAll();
     }
-    else if (command->getName().compare("PRINT VAL") == 0){
+    else if (command->getName() == "PRINT VAL"){
         printVal(command->getArguments());
     }
-    else if (command->getName().compare("PRINT EXPR") == 0){
+    else if (command->getName() == "PRINT EXPR"){
         printExpr(command->getArguments());
     }
-    else if (command->getName().compare("++") == 0){
+    else if (command->getName() == "++"){
         increase(command->getArguments());
     }
-    else if (command->getName().compare("--") == 0){
+    else if (command->getName() == "--"){
         decrease(command->getArguments());
     }
-    else if (command->getName().compare("EXIT") == 0){
+    else if (command->getName() == "EXIT"){
         exit = true;
     }
     else {
@@ -47,76 +46,67 @@ void Executor::execute()
     delete command;
 }
 
-/**
- * @brief check is EXIT flag is up
- * 
- * @return true the program will exit
- * @return false the program won't exit
- */
-bool Executor::isExit() {
+
+/// Getter for exit flag
+/// \return if the program should be terminated
+bool Executor::isExit() const {
     return exit;
 }
 
-/**
- * @brief execute SET command
- * 
- * @param args command arguments
- */
-void Executor::set(vector<string>& args) {
+
+/// SET command
+/// \param args SET command parameters
+void Executor::set(vector<string>& args) const {
     const char* add = args[0].c_str();
     Address address = Address(add,
         	                  Table::instance()->getCurrentAddress().row,
                               Table::instance()->getCurrentAddress().column);
+
     Table::instance()->setCell(address, args[1]);
     Table::instance()->setCurrentAddress(address);
+
     std::cout << "Set the value of " << args[0] << " to " << args[1] << "\n";
 }
 
-/**
- * @brief execute PRINT VAL command
- * 
- * @param args command arguments
- */
-void Executor::printVal(vector<string>& args) {
+
+/// PRINT VAL command
+/// \param args PRINT VAL command parameters
+void Executor::printVal(vector<string>& args) const {
     const char* add = args[0].c_str();
     Address address = Address(add,
                               Table::instance()->getCurrentAddress().row,
                               Table::instance()->getCurrentAddress().column);
+
     Table::instance()->setCurrentAddress(address);
-    std::cout << args[0]  
-              << " = "
-              << Table::instance()->getCellValue(address)
-              << "\n";
+
+    std::cout << args[0] << " = " << Table::instance()->getCellValue(address) << "\n";
 }
 
-/**
- * @brief execute PRINT EXPR command
- * 
- * @param args command arguments
- */
-void Executor::printExpr(vector<string>& args) {
+
+/// PRINT EXPR command
+/// \param args PRINT EXPR command parameters
+void Executor::printExpr(vector<string>& args) const {
     const char* add = args[0].c_str();
     Address address = Address(add,
         	                  Table::instance()->getCurrentAddress().row,
                               Table::instance()->getCurrentAddress().column);
+
     Table::instance()->setCurrentAddress(address);
-    std::cout << args[0] 
-              << " = "
-              << Table::instance()->getCell(address)
-              << "\n";
+
+    std::cout << args[0] << " = " << Table::instance()->getCell(address) << "\n";
 }
 
-/**
- * @brief execute SAVE command
- * 
- * @param args command arguments
- */
-void Executor::save(vector<string>& args) {
+
+/// SAVE command
+/// \param args SAVE command parameters
+void Executor::save(vector<string>& args) const {
+
     Table::instance()->setCurrentAddress(Address(0, 0));
+
     std::ofstream file(args[0]);
     if (file.is_open()) {
         for (int r = 1; r <= Table::instance()->getMaxAddress().row; r++) {
-            std::string line = "";
+            std::string line;
 
             for (int c = 1; c <= Table::instance()->getMaxAddress().column; c++) {
                 Address address(r,c);
@@ -135,28 +125,30 @@ void Executor::save(vector<string>& args) {
     }
 }
 
-/**
- * @brief execute LOAD command
- * 
- * @param args command arguments
- */
-void Executor::load(vector<string>& args) {
+
+/// LOAD command
+/// \param args LOAD command parameters
+void Executor::load(vector<string>& args) const {
+
     Table::instance()->setCurrentAddress(Address(0, 0));
+
     std::ifstream file(args[0]);
     if (file.is_open()) {
         string line;
-        int p, r = 1;
+        int position, row = 1;
+
         while (std::getline(file, line))
         {
-            int c = 1;
-            while ((p = line.find(",")) != std::string::npos || (p = line.find(";")) != std::string::npos) {
-                Address address(r, c);
-                std::string expr = line.substr(0, p);
-                line.erase(0, p+1);
+            int column = 1;
+            while ((position = line.find(',')) != std::string::npos ||
+                   (position = line.find(';')) != std::string::npos) {
+                Address address(row, column);
+                std::string expr = line.substr(0, position);
+                line.erase(0, position+1);
                 Table::instance()->setCell(address, expr);
-                c++;
+                column++;
             }
-            r++;
+            row++;
         }
         file.close();
     }
@@ -165,19 +157,19 @@ void Executor::load(vector<string>& args) {
     }
 }
 
-/**
- * @brief execute ++ command
- * 
- * @param args command arguments
- */
-void Executor::increase(vector<string>& args) {
+
+/// ++ command
+/// \param args ++ command arguments
+void Executor::increase(vector<string>& args) const {
     const char* add = args[0].c_str();
+
     Address address = Address(add,
         	                  Table::instance()->getCurrentAddress().row,
                               Table::instance()->getCurrentAddress().column);
+
     string value = Table:: instance()->getCell(address);
 
-    if (value == "" || value[0] == '\"') {
+    if (value.empty() || value[0] == '\"') {
         throw std::invalid_argument("Error: Cannot increase cell.");
     }
 
@@ -186,30 +178,33 @@ void Executor::increase(vector<string>& args) {
         isNegative = true;
         value.erase(value.begin() + 0);
     }
+
     if (value.find_first_not_of("0123456789") == std::string::npos) {
         int i = std::stoi(value);
         if (isNegative) {i *= (-1);}
+
         Table:: instance()->setCell(address, std::to_string(i+1));
     }
     else {
         throw std::invalid_argument("Error: Cannot increase cell.");
     }
+
     Table::instance()->setCurrentAddress(address);
 }
 
-/**
- * @brief execute -- command
- * 
- * @param args command arguments
- */
-void Executor::decrease(vector<string>& args) {
+
+/// -- command
+/// \param args -- command parameters
+void Executor::decrease(vector<string>& args) const {
     const char* add = args[0].c_str();
+
     Address address = Address(add,
         	                  Table::instance()->getCurrentAddress().row,
                               Table::instance()->getCurrentAddress().column);
+
     string value = Table:: instance()->getCell(address);
 
-    if (value == "" || value[0] == '\"') {
+    if (value.empty() || value[0] == '\"') {
         throw std::invalid_argument("Error: Cannot increase cell.");
     }
 
@@ -218,6 +213,7 @@ void Executor::decrease(vector<string>& args) {
         isNegative = true;
         value.erase(value.begin() + 0);
     }
+
     if (value.find_first_not_of("0123456789") == std::string::npos) {
         int i = std::stoi(value);
         if (isNegative) {i *= (-1);}
@@ -229,19 +225,20 @@ void Executor::decrease(vector<string>& args) {
     Table::instance()->setCurrentAddress(address);
 }
 
-/**
- * @brief execute PRINT EXPR ALL command
- * 
- */
-void Executor::printExprAll() {
+
+/// PRINT EXPR ALL command
+void Executor::printExprAll() const {
     Table::instance()->setCurrentAddress(Address(0, 0));
+
     Address max = Table::instance()->getMaxAddress();
+
     int iterations = max.column / 5;
     int lastIteration = max.column % 5;
 
     for (int i = 0; i < iterations;  i++) {
         printColumnLine(i, 5);
         std::cout << "\n";
+
         for (int r = 1; r <= max.row; r++) {
             std::cout << "R" << r;
             for (int c = 1; c <= 5; c++) {
@@ -260,6 +257,7 @@ void Executor::printExprAll() {
     std::cout << "\n";
     for (int r = 1; r <= max.row; r++) {
         std::cout << "R" << r;
+
         for (int c = 1; c <= lastIteration; c++) {
             Address address(r, c+iterations*5);
             std::cout << "|";
@@ -271,16 +269,15 @@ void Executor::printExprAll() {
     }
 }
 
-/**
- * @brief execute PRINT VAL ALL command
- * 
- */
-void Executor::printValAll() {
+
+/// PRINT VAL ALL
+void Executor::printValAll() const {
     Address max = Table::instance()->getMaxAddress();
     int iterations = max.column / 5;
     int lastIteration = max.column % 5;
 
     for (int i = 0; i < iterations;  i++) {
+
         printColumnLine(i, 5);
         std::cout << "\n";
         for (int r = 1; r <= max.row; r++) {
@@ -302,6 +299,7 @@ void Executor::printValAll() {
     std::cout << "\n";
     for (int r = 1; r <= max.row; r++) {
         std::cout << "R" << r;
+
         for (int c = 1; c <= lastIteration; c++) {
             Address address(r, c+iterations*5);
             Table::instance()->setCurrentAddress(address);
@@ -315,37 +313,38 @@ void Executor::printValAll() {
 }
 
 
-/**
- * @brief print expression in cell
- *
- * @param address the address of the cell
- */
-void Executor::printExpression(Address address) {
+/// PRINT EXPR command
+/// \param address address of the cell to print
+void Executor::printExpression(Address address) const {
     string value = Table::instance()->getCell(address);
     value = (value.length() > 10) ? "LONG CELL" : value;
     std::cout << value;
 }
 
-/**
- * @brief print value of cell
- *
- * @param address the address of the cell
- */
-void Executor::printValue(Address address) {
+
+/// PRINT VAL command
+/// \param address address of the cell to print
+void Executor::printValue(Address address) const {
     string value = std::to_string(Table::instance()->getCellValue(address));
     value = (value.length() > 10) ? "LONG CELL" : value;
     std::cout << value;
 }
 
-/**
- * @brief print the column table header
- *
- * @param iteration the iteration of the table the header is for
- * @param times times to print the column
- */
-void Executor::printColumnLine(int iteration, int times) {
+
+/// Print table header
+/// \param iteration the iteration of the table the header is for
+/// \param times times to print the column
+void Executor::printColumnLine(int iteration, int times) const {
     for (int c = 1; c <= times; c++) {
         std::cout << std::setw(12);
         std::cout <<  "C" << c + iteration * 5;
     }
+}
+
+
+/// Instance getter for singleton
+/// \return instance of the Executor class
+Executor *Executor::instance() {
+    static Executor i;
+    return &i;
 }
