c1a285eecfa29d4206e769da1db84b1c5f39b504
Veneta <venkireva80@gmail.com>
2022-07-14 00:25:53 +0300

    Make whitespace consistent after REQUIRE

--- test/tests.cpp
+++ test/tests.cpp
@@ -82,81 +82,81 @@ TEST_CASE("Shunting-yard algorithm")
 
     SECTION("Less than") {
         const char* test1 = "5 < 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 0);
 
         const char* test2 = "3 < 5";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 1);
     }
 
     SECTION("Greater than") {
         const char* test1 = "5 > 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 1);
 
         const char* test2 = "3 > 5";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 0);
     }
 
     SECTION("Equal") {
         const char* test1 = "5 == 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 0);
 
         const char* test2 = "5 == 5";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 1);
 
         const char* test3 = "5 = = 5";
-        REQUIRE_THROWS (ExpressionHandler::instance()->shuntingYard(test3));
+        REQUIRE_THROWS(ExpressionHandler::instance()->shuntingYard(test3));
     }
 
     SECTION("Not equal") {
         const char* test1 = "5 != 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 1);
 
         const char* test2 = "5 != 5";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 0);
 
         const char* test3 = "5 ! = 5";
-        REQUIRE_THROWS (ExpressionHandler::instance()->shuntingYard(test3));
+        REQUIRE_THROWS(ExpressionHandler::instance()->shuntingYard(test3));
     }
 
     SECTION("And") {
         const char* test1 = "5 and 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 1);
 
         const char* test2 = "5 and 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 0);
 
         const char* test3 = "0 and 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test3) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test3) == 0);
     }
 
     SECTION("Or") {
         const char* test1 = "5 or 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 1);
 
         const char* test2 = "5 or 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 1);
 
         const char* test3 = "0 or 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test3) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test3) == 0);
     }
 
     SECTION("Not") {
         const char* test1 = "not 3";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 0);
 
         const char* test2 = "not 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 1);
 
         const char* test3 = "not (3+5)";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test3) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test3) == 0);
     }
 
     SECTION("If") {
         const char* test1 = "if 5==5 ? 2 : 3;";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 2);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 2);
 
         const char* test2 = "if 5==3 ? 2 : 3;";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 3);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 3);
     }
 
 
@@ -191,38 +191,38 @@ TEST_CASE("Shunting-yard algorithm")
 
     SECTION("Complex expressions - value") {
         const char* test1 = "5/3*(5/2)-5*(4*3/(2))";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == (-20));
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == (-20));
     }
 
     SECTION("Complex expressions - logic") {
         const char* test1 = "5/3*(5/2)>5*(4*3/(2))";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test1) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test1) == 0);
 
         const char* test2 = "5/3*(5/2)<5*(4*3/(2))";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test2) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test2) == 1);
 
         const char* test3 = "5/3*(5/2) == 5*(4*3/(2))";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test3) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test3) == 0);
 
         const char* test4 = "5/3*(5/2) != 5*(4*3/(2))";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test4) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test4) == 1);
 
         const char* test5 = "((4*9)/3)*5 and 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test5) == 0);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test5) == 0);
 
         const char* test6 = "(3+5)*42 and 1";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test6) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test6) == 1);
 
         const char* test7 = "8*5 or 0";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test7) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test7) == 1);
 
         const char* test8 = "7*6 or 1";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test8) == 1);
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test8) == 1);
     }
 
     SECTION("Whitespace expressions") {
         const char* test = "5  /  3  * (   5 /   2 ) -5 *( 4* 3 /( 2))";
-        REQUIRE (ExpressionHandler::instance()->shuntingYard(test) == (-20));
+        REQUIRE(ExpressionHandler::instance()->shuntingYard(test) == (-20));
     }
 }
 
