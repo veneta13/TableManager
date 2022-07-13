bca3f41fa4f260860cb98dd7a9141586db2a3183
Veneta <venkireva80@gmail.com>
2022-07-13 21:44:29 +0300

    Add helper methods for Address and update documentation

--- inc/table_address.h
+++ inc/table_address.h
@@ -1,26 +1,25 @@
-#ifndef __TABLECELL_H__
-#define __TABLECELL_H__
+#ifndef __ADDRESS_H__
+#define __ADDRESS_H__
 
 #include <cctype>
 #include <stdexcept>
 #include <string>
 
-/**
- * @brief adress container
- * 
- */
-struct Address{
+/// Represents a table cell address
+struct Address {
     int row; //row in table
     int column; //column in table 
 
-    Address() = default;
-    Address(int row, int column) : row(row), column(column) {};
-    Address (const char *& text);
-    Address (const char *& text, int currentRow, int currentColumn);
-    bool operator==(const Address &a) const;
+    Address();
+    Address(int row, int column);
+    Address(const char *& text);
+    Address(const char *& text, int currentRow, int currentColumn);
+
+    bool operator==(Address const& other) const;
 
 private:
     int getNumber(const char *& text);
+    void readAddress(char const*& text, int& current, int& updated);
 };
 
-#endif // __TABLECELL_H__
+#endif // __ADDRESS_H__
--- src/table_address.cpp
+++ src/table_address.cpp
@@ -1,79 +1,18 @@
 #include "../inc/table_address.h"
 
-/**
- * @brief compare two addresses
- * 
- * @param a another address
- * @return true the addresses are equal
- * @return false the addresses are not equal
- */
-bool Address :: operator==(const Address &a) const {
-    return column == a.column && row == a.row;
-}
 
-/**
- * @brief constructor
- * 
- * @param text the value of the cell
- * @param currentRow current row (in case of relative indexing)
- * @param currentColumn current column (in case of relative indexing)
- * @return Address 
- */
-Address :: Address (const char *& text, int currentRow, int currentColumn) {
-    if (*text == 'R') {
-        text++;
-        if (*text == '['){
-            text++;
-            if (*text == '-') {
-                text++;
-                row = currentRow - getNumber(text);
-            }
-            else {
-                row = currentRow + getNumber(text);
-            }
-            if (*text == ']') {
-                text++;
-            }
-            else {
-                throw std::runtime_error("Error: wrong relative addressing!");
-            }
-        }
-        else {
-            row = getNumber(text);
-        }
-    }
-    else {throw std::runtime_error("Invalid row address");}
-    if (*text == 'C') {
-        text++;
-        if (*text == '['){
-            text++;
-            if (*text == '-') {
-                text++;
-                column = currentColumn - getNumber(text);
-            }
-            else {
-                column = currentColumn + getNumber(text);
-            }
-            if (*text == ']') {
-                text++;
-            }
-            else {
-                throw std::runtime_error("Error: wrong relative addressing!");
-            }
-        }
-        else {
-            column = getNumber(text);
-        }
-    }
-    else {throw std::runtime_error("Invalid column address");}
-}
+/// Default constructor
+Address::Address() = default;
 
-/**
- * @brief constructor
- * 
- * @param text the value of the cell
- * @return Address the address of the cell
- */
+
+/// Constructor with parameters
+/// \param _row row index
+/// \param _column column index
+Address::Address(int _row, int _column) : row(_row), column(_column) {}
+
+
+/// Constructor with parameters
+/// \param text text to read address from
 Address :: Address (const char *& text) {
     if (*text == 'R') {
         text++;
@@ -88,12 +27,37 @@ Address :: Address (const char *& text) {
     else {throw std::runtime_error("Invalid column address");}
 }
 
-/**
- * @brief get integer value of string 
- * 
- * @param text the text
- * @return int the value
- */
+
+/// Constructor with parameters
+/// \param text text to read address from
+/// \param currentRow current row (in case of relative indexing)
+/// \param currentColumn current column (in case of relative indexing)
+Address :: Address (const char *& text, int currentRow, int currentColumn) {
+    if (*text == 'R') {
+        readAddress(text, currentRow, row);
+    }
+    else {
+        throw std::runtime_error("Invalid row address");
+    }
+
+    if (*text == 'C') {
+        readAddress(text, currentColumn, column);
+    }
+    else {throw std::runtime_error("Invalid column address");}
+}
+
+
+/// Compare addresses with equality operator
+/// \param other address to compare to
+/// \return if the addresses are equal
+bool Address :: operator==(Address const& other) const {
+    return column == other.column && row == other.row;
+}
+
+
+/// Get integer value of text
+/// \param text text to read number from
+/// \return integer value of the text
 int Address :: getNumber (const char *& text) {
     int number = 0;
     while (std::isdigit(*text)) {
@@ -102,4 +66,35 @@ int Address :: getNumber (const char *& text) {
         text++;
     }
     return number;
+}
+
+
+/// Extract address from text
+/// \param text text to extract address from
+/// \param current currently opened cell address
+/// \param updated new cell address
+void Address::readAddress(char const*& text, int& current, int& updated) {
+    text++;
+
+    if (*text == '['){
+        text++;
+
+        if (*text == '-') {
+            text++;
+            updated = current - getNumber(text);
+        }
+        else {
+            updated = current + getNumber(text);
+        }
+
+        if (*text == ']') {
+            text++;
+        }
+        else {
+            throw std::runtime_error("Error: wrong relative addressing!");
+        }
+    }
+    else {
+        updated = getNumber(text);
+    }
 }
